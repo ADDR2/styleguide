bfa6f748ca5473ceac8a16f7ba3a004aaab9a200
Miguel Vizcaino <miguel87831@gmail.com>
2015-04-22 12:10:58 -0430

    STYL-1 add validate whit jscs and node

--- validateStyle.js
+++ validateStyle.js
@@ -3,11 +3,10 @@ var exec = require('child_process').exec;
 function validateJS(filesName) {
     var re = /(?:\.([^.]+))?$/;
 
-
     if (filesName.length > 0) {
         filesName.forEach(function(item) {
             if (re.exec(item)[1] === 'js') {
-                exec('jscs ' + item, function (error, stdout, stderr) {
+                exec('jscs ' + item, function(error, stdout, stderr) {
                     if (!stdout.search('No code style errors found.')) {
                         console.log(item + '- style approved');
                     }else {
@@ -23,5 +22,6 @@ function validateJS(filesName) {
 exec('git diff --name-only --cached', function(error, stdout, stderr) {
     var filesName;
     filesName = stdout.split('\n');
+    console.log("aja");
     validateJS(filesName);
 });
