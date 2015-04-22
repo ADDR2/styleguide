a6c289c4aead0643a4e213bbda23b835f308594e
Miguel Vizcaino <miguel87831@gmail.com>
2015-04-22 09:47:08 -0430

    test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 exec('git diff --name-only --cached', function (error, stdout, stderr) {
 	console.log("empieza aqui");
-	console.log(stdout.split("&lt;br&gt;"));
+	console.log(stdout.split(" "));
 		
   //  if(stdout.search("console.log") !== -1) {
   //  		console.log("remove console.log in js files");
