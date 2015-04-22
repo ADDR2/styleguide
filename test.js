0436970bf22bbb1ea09e2540ed5df0d757d37c8c
Miguel Vizcaino <miguel87831@gmail.com>
2015-04-22 11:40:30 -0430

    test

--- test.js
+++ test.js
@@ -1,31 +1,26 @@
 var exec = require('child_process').exec;
 
-
 function validateJS(filesName) {
-	var re = /(?:\.([^.]+))?$/;
+    var re = /(?:\.([^.]+))?$/;
 
-	if(filesName.length > 0) {
-		filesName.forEach(function(item) {
-			if(re.exec(item)[1] === "js") {
-				exec('jscs '+item, function (error, stdout, stderr) {
-					if(!stdout.search("No code style errors found.")) {
-						console.log("no errors");
-					}else {
-						console.log("errors");
-					}
-				});
-			}
-		});
-	}
-
-	// exec('jscs', function (error, stdout, stderr) {
-	// });
+    if (filesName.length > 0) {
+        filesName.forEach(function(item) {
+            if (re.exec(item)[1] === 'js') {
+                exec('jscs ' + item, function(error, stdout, stderr) {
+                    if (!stdout.search('No code style errors found.')) {
+                        console.log(item + '- style approved');
+                    }else {
+                        console.log(stdout);
+                        process.exit(1);
+                    }
+                });
+            }
+        });
+    }
 }
 
-exec('git diff --name-only --cached', function (error, stdout, stderr) {
-	var filesName;
-	filesName = stdout.split("\n");
-	validateJS(filesName);	
-
-		// process.exit(1);
+exec('git diff --name-only --cached', function(error, stdout, stderr) {
+    var filesName;
+    filesName = stdout.split('\n');
+    validateJS(filesName);
 });
